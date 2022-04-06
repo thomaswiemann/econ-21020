a955fe40d89cb7779cec8b02af9968f7ff0ce705
thomaswiemann <wiemann@uchicago.edu>
2022-04-06 12:39:02 -0500

    add plots for lecture 3A

--- lecture_plots.R
+++ lecture_plots.R
@@ -48,3 +48,67 @@ plt <- ggplot() +
   theme_classic(base_size = 20) +
   theme(text = element_text(size = 20, family="Times"))
 save_img(plt, "figures/lectures/lecture_2A_chi2pdf.pdf", w = 8, h = 7)
+
+# Lecture: Review of Statistics ================================================
+
+# Set true parameters of the distributions, the sample size
+mu <- 1
+sgm <- 1 
+nobs <- 10
+
+# Simulate distribution of estimators from Lecture 3A Example 5
+nsim <- 10000
+est_mat <- matrix(0, nsim, 4)
+lambda <- 1
+for (s in 1:nsim) {
+  # Simulate a sample of size nobs
+  data_s <- rnorm(nobs, mu, sgm)
+  # Compute estimators
+  est_mat[s, 2] <- data_s[1]
+  est_mat[s, 3] <- mean(data_s)
+  est_mat[s, 4] <- sum(data_s) / (nobs + lambda)
+}#FOR
+
+# Plot distributions of estimators
+plt <- ggplot() + 
+  geom_histogram(aes(x = est_mat[, 2], y =..density..), bins = 80) +
+  annotate(geom="text", x=mu + 0.1, y=1, label="mu", 
+           parse = TRUE,  color="red", size = 10) +
+  geom_vline(xintercept = mu, col = "red") + 
+  ylab("Density") + 
+  xlab(expression(hat(mu)[n]^(2))) +
+  xlim(c(-2, 4)) +
+  ylim(c(0, 1.5)) +
+  theme_classic(base_size = 20) +
+  theme(text = element_text(size = 20,  family="Times"))
+save_img(plt, "figures/lectures/lecture_2A_chi2pdf.pdf", w = 8, h = 7)
+
+ggplot() + 
+  geom_histogram(aes(x = est_mat[, 3], y =..density..), bins = 80) +
+  geom_vline(xintercept = mu, col = "red") +
+  ylab("Density") + 
+  xlab(expression(hat(mu)[n]^(3))) +
+  xlim(c(-2, 4)) +
+  ylim(c(0, 1.5)) +
+  theme_classic(base_size = 20) +
+  theme(text = element_text(size = 20,  family="Times"))
+
+ggplot() + 
+  geom_histogram(aes(x = est_mat[, 4], y =..density..), bins = 80) +
+  geom_vline(xintercept = mu, col = "red") +
+  ylab("Density") + 
+  xlab(expression(hat(mu)[n]^(4))) +
+  xlim(c(-2, 4)) +
+  ylim(c(0, 1.5)) +
+  theme_classic(base_size = 20) +
+  theme(text = element_text(size = 20,  family="Times"))
+
+# Calculate Mean-Squared-Error
+squared_errors <- (est_mat - mu)^2
+mse_by_est <- apply(squared_errors, 2, mean)
+#> [1] 1.00000000 0.97166468 0.10052857 0.09188914
+
+
+
+
+
